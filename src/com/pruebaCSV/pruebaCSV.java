db1ce2bad1c95d8abb42d4173691b0f7e6293267
Juan Chinchilla <9001239@alumnos.ufv.es>
2022-10-26 19:41:59 +0200

    Librerias Añadidas

--- src/com/pruebaCSV/pruebaCSV.java
+++ src/com/pruebaCSV/pruebaCSV.java
@@ -2,5 +2,7 @@ package com.pruebaCSV;
 
 public class pruebaCSV {
         public static void main(String[] args) {
+                LectorCSV milector = new LectorCSV();
+            milector.leeCSV("datos/pokemon.csv");
         }
 }
